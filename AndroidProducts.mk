8807c5e7f6e90c1354625e2c5f8b8e14f2868590
VortexHD <vortexhdwhite@gmail.com>
2021-05-03 13:03:36 +0100

    h850: Dot-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,8 +14,8 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_h850.mk
+    $(LOCAL_DIR)/dot_h850.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_h850-userdebug \
-    lineage_h850-eng
+    dot_h850-userdebug \
+    dot_h850-eng
--- dot_h850.mk
+++ dot_h850.mk
@@ -18,15 +18,19 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common DotOS stuff.
+$(call inherit-product, vendor/dot/config/common.mk)
 
-# Inherit from h815 device
+# Inherit from h850 device
 $(call inherit-product, device/lge/h850/device.mk)
+
+# DotOS variables
+#WITH_GAPPS := true
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := h850
-PRODUCT_NAME := lineage_h850
+PRODUCT_NAME := dot_h850
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-H850
 PRODUCT_MANUFACTURER := LGE
